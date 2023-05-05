b64f078339d4b9f722f1f6467f0ed998f04ac297
HyeongBeenYeom <yhb90816@gmail.com>
2023-05-05 16:39:12 +0900

    ADD N과 M (5)

---
+++ back tracking/ps_15654.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int N,M;
+int arr[10];
+vector<int> v;
+bool visit[10];
+
+void DFS(int cnt){
+    if(cnt == M){
+        for(int i=0;i<M;i++){
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i=0; i<N;i++){
+        if(!visit[i]){
+            visit[i] = true;
+            arr[cnt] = v[i];
+            DFS(cnt+1);
+            visit[i] = false;   
+        }
+        
+    }
+}
+
+int main(){
+
+    int a;
+    cin >> N >> M;
+    for(int i=0;i<N;i++){
+        cin >> a;
+        v.push_back(a);
+    }
+    sort(v.begin(), v.end());
+
+    DFS(0);
+
+    return 0;
+}
